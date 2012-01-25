2d92951b8e9d16eb995f30d6eed7ae22f774894e
Mats Rauhala <mats.rauhala@gmail.com>
2012-01-25 11:24:47 +0200

    Rewrite

--- demolaskuri.scm
+++ demolaskuri.scm
@@ -1,25 +1,49 @@
-(use srfi-69)
-
-(define *db* (make-hash-table))
+(use format sqlite3 matchable)
 
 (define-record course
-               name times excs done)
+               name
+               done
+               total-exc
+               required)
 
-(define (define-course name times excs)
-  (make-course name times excs 0))
+(define (create-course name done total-exc required)
+  (define (with-default x def)
+    (if (sql-null#sql-null? x) def x))
+  (make-course
+    (with-default name "")
+    (with-default done 0)
+    (with-default total-exc 0)
+    (with-default required 0)))
 
-(define (course-percentage course)
-  (let ((total (* (course-times course) (course-excs course))))
-    (string-append 
-      (number->string ( * 100  (/ (course-done course) total)))
-      "%")))
+(define *db* (open-database "demonstrations.db"))
 
-(define (save-course course)
-  (hash-table-set! *db* (course-name course) course))
+(define (init-db)
+  (execute *db* "create table if not exists courses (name primary key, times, excs, required)")
+  (execute *db* "create table if not exists demonstrations (name, done, timestamp default current_timestamp, foreign key (name) references demonstrations(name))"))
 
-(define (mark-demonstrations name n)
-  (hash-table-update! *db*
-                      name 
-                      (lambda (course)
-                        (course-done-set! course (+ (course-done course) n))
-                        course)))
+(init-db)
+
+(define (add-course! name demonstrations excercises required)
+  (execute *db*
+           "insert or ignore into courses (name, times, excs, required) values (?, ?, ?, ?)"
+           name demonstrations excercises required))
+
+(define (mark-as-done! name n)
+  (execute *db*
+           "insert into demonstrations (name, done) values (?, ?)"
+           name n))
+
+(define (get-course! name)
+  (row-to-course ( first-row *db* "select name, (select sum(done) from demonstrations where name=courses.name) as done, (times * excs * 1.0) as total_exc, required from courses where name=?" name)))
+
+(define (get-courses!)
+  (map-row create-course *db* "select name, (select sum(done) from demonstrations where name=courses.name) as done, (times * excs * 1.0) as total_exc, required from courses"))
+
+(define (row-to-course row)
+  (apply create-course row))
+
+(define (enough-done? course)
+  (let ((done (course-done course)) 
+        (total (course-total-exc course))
+        (required (course-required course)))
+    (> (/ (if (sql-null#sql-null? done) 0 done) total) required)))
